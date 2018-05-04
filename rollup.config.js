11a234cbbc630df68c0c40c16df7b651fa5e584f
Bibliofile <bibliofilegit@gmail.com>
2018-05-03 21:05:08 -0600

    Update dependencies, improve build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import string from 'rollup-plugin-string'
+import uglify from 'rollup-plugin-uglify-es'
 
 // Build notes:
 // Ignore the warnings about the 'crypto' module, it isn't used.
@@ -10,12 +11,14 @@ export default {
     input: 'src/index.ts',
     output: {
         file: 'bundle.js',
-        format: 'iife'
+        format: 'iife',
+        sourcemap: true
     },
     plugins: [
         resolve(),
         commonjs(),
         string({ include: '**/*.html' }),
         typescript(),
+        uglify()
     ]
 }
