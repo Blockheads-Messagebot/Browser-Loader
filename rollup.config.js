d667b69d4d1ada8995f9e72e741013a604bb487b
Bibliofile <bibliofilegit@gmail.com>
2019-02-27 20:30:23 -0700

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
-import string from 'rollup-plugin-string'
+import { string } from 'rollup-plugin-string'
 import { terser } from 'rollup-plugin-terser'
 
 // Build notes:
